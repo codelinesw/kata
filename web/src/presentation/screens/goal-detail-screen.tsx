ae5f1b3894ec9bb08ca29902d18bfcb3cc0f72e3
codelinesw <codelinesw@gmail.com>
2026-08-21 20:18:04 -0500

    Se agregan los eventos y funcionalidades que permite actualizar el monto actual, es decir el dinero que tiene ahorrado en el momento, sumando lo que tenia + lo nuevo, adicional este impacto afecta al estado en Redux para que el cambio sea visible en las listas, también se actualiza desde el front en la web usando la tecnica de la referencia para evitar re-renders

--- web/src/presentation/screens/goal-detail-screen.tsx
+++ web/src/presentation/screens/goal-detail-screen.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { PopUpAbono } from "../components/pop-up/pop-up-component";
 import type { GoalDetailProps } from "../props/gaol-detail";
 import { styles } from "./goal-detail-style";
@@ -7,29 +7,47 @@ import { styles } from "./goal-detail-style";
 export const GoalDetailScreen: React.FC<GoalDetailProps> = ({
     name,
     currentAmount,
-    targetAmount
+    targetAmount,
+    addPayment
 }) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const currentAmountRef = useRef<HTMLSpanElement>(null);
+    const updateCurrentAmountDOM = (newTarget: number) => {
+        if (currentAmountRef.current) {
+            let _currentAmountRef = Number(currentAmountRef.current?.textContent.replaceAll("$","") || '0');
+            if (_currentAmountRef > 0) {
+                currentAmountRef.current.innerText = `$${_currentAmountRef+newTarget}`;
+            } else {
+                currentAmountRef.current.innerText = `$${currentAmount+newTarget}`;
+            }    
+        }
+    };
     return (
         <div style={styles.body}>
             <h2>Detalle de tu ahorro</h2>
             <div style={styles.containerItem}>
                 <h3>Meta: {name || ""}</h3>
-                <p>Ahorrado actualmente: <strong>${currentAmount || "0"}</strong></p>
+                <p>Ahorrado actualmente:
+                    <strong ref={currentAmountRef}>
+                        ${currentAmount || "0"}
+                    </strong>
+                </p>
                 <p>Meta total: <strong>${targetAmount || "0"}</strong></p>
                 <button onClick={() => {
                     setIsOpen(true)
                 }}>Agregar abono</button>
             </div>
-            <PopUpAbono 
-                isOpen={isOpen} 
+            <PopUpAbono
+                isOpen={isOpen}
                 onClose={() => {
                     setIsOpen(false)
-                }} 
-                onSubmit={() => {
-                    alert("la freezer")
-                    setIsOpen(false)
-                }} 
+                }}
+                onSubmit={(amount: number) => {
+                    addPayment(amount);
+                    updateCurrentAmountDOM(amount);
+                    setIsOpen(false);
+                    alert("El abono se agregó con exíto")
+                }}
             />
         </div>
     )
